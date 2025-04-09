4fabe8d63aa2d30e9e1ab569134c9570c437cded
Guillermo A. Perez <gaperez64@gmail.com>
2025-04-09 21:07:41 +0200

    have solutions, but need min rank solutions

--- systems/lineqs.py
+++ systems/lineqs.py
@@ -1,4 +1,7 @@
+import z3
+
 type Mat = tuple[tuple[int, ...], ...]
+type Vecs = list[tuple[int, ...]]
 
 
 class LinEqs:
@@ -18,3 +21,51 @@ class LinEqs:
                     s += " + " + str(c) + "x" + str(i)
             s += "\n"
         return s
+
+    def solutions(self):
+        assert len(self.A) != 0
+        nvars = len(self.A[0]) - 1
+        indets = [z3.Int("x" + str(i)) for i in range(nvars)]
+        s = z3.Solver()
+        # Make sure we focus on nonnegative values
+        s.add(z3.And([indets[i] >= 0 for i in range(nvars)]))
+
+        # First we get the periods
+        s.push()
+        # make sure we get nonzero solutions
+        s.add(z3.Or([indets[i] >= 1 for i in range(nvars)]))
+        periods = []
+        needs_bases = False
+        for a in self.A:
+            linpoly = 0
+            for i, c in enumerate(a):
+                if i == len(a) - 1:
+                    needs_bases = needs_bases or (c != 0)
+                    s.add(linpoly == 0)
+                else:
+                    linpoly += str(c) * indets[i]
+        while s.check() != z3.unsat:
+            m = s.model()
+            vec = tuple([m[indets[i]] for i in range(nvars)])
+            periods.append(vec)
+            s.add(z3.Or([indets[i] < vec[i] for i in range(nvars)]))
+        s.pop()
+
+        # Get bases
+        if not needs_bases:
+            return [tuple([0] * nvars)], periods
+        bases = []
+        for a in self.A:
+            linpoly = 0
+            for i, c in enumerate(a):
+                if i == len(a) - 1:
+                    s.add(linpoly == (-1 * c))
+                else:
+                    linpoly += str(c) * indets[i]
+        while s.check() != z3.unsat:
+            m = s.model()
+            vec = tuple([m[indets[i]] for i in range(nvars)])
+            bases.append(vec)
+            s.add(z3.Or([indets[i] < vec[i] for i in range(nvars)]))
+        # TODO compute bases
+        return bases, periods
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from systems.lindivs import LinDivs
+from systems.lineqs import LinEqs
 
 
 def main():
@@ -7,6 +8,10 @@ def main():
                   ((10, 20, 30), ),
                   ((11, 22, 33), ))
     print(str(lds))
+
+    leqs = LinEqs(tuple([(1, -1, 0, 0), (0, 1, -2, 0)]))
+    print(str(leqs))
+    leqs.solutions()
 
 
 if __name__ == "__main__":
